8b2c443887ac6aaf6e17211c27a7ca8a4ef892dc
MineCopre <diogoah99@gmail.com>
2021-03-31 16:34:33 +0100

    testing captcha9

--- public_html/PHP/form.php
+++ public_html/PHP/form.php
@@ -6,41 +6,38 @@
 	
 	require_once "../../vendor/autoload.php";
 	
-	$captchaURL = "https://www.google.com/recaptcha/api/siteverify?secret=6LfmF8AZAAAAAF284CXnmOJzsaZ8hZ-e9TQfHHIz";
-	$response = file_get_contents($url);
-	$responseKeys = json_decode($response, true);
-
+	
 	if (isset($_POST['g-recaptcha-response'])) {
+		
 		$captcha = $_POST['g-recaptcha-response'];
-		echo "yay";
-	} else {
-		$captcha = false;
-		echo "yay2";
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$secret = '6LfmF8AZAAAAAF284CXnmOJzsaZ8hZ-e9TQfHHIz';
+		$response = file_get_contents(
+        "https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $ip);
+		$response = json_decode($response);
+		
+		if ($response->success === false) {
+			echo "nay";
+		}
+		else{
+			if($response->score <= 0.5){
+				echo "yay";
+			}
+			else{
+				echo "nay2";
+			}
+		}
 	}
 	
-	if ($responseKeys["success"] && $responseKeys["action"] == 'contactForm') {
-	echo "yo";
-            if ($responseKeys["score"] >= 0.5) {
-                echo "yay";
-            } elseif ($responseKeys["score"] < $g_recaptcha_allowable_score) {
-                //failed spam test. Offer the visitor the option to try again or use an alternative method of contact.
-            }
-        } elseif($responseKeys["error-codes"]) { //optional
-            //handle errors. See notes below for possible error codes
-            //(I handle errors by sending myself an email with the error code for debugging and offering the visitor the option to try again or use an alternative method of contact)
-        } else {
-            //unkown screw up. Again, offer the visitor the option to try again or use an alternative method of contact.
-        }
-
-        exit;
-	
-	if ($response->success==true && $response->score <= 0.5) {
-    //Do something to denied access
-}
+	else {
+		
+	}
 	
 	
 	
-		try {
+	
+	
+	try {
 		
 		$mail->isSMTP();
 		
@@ -67,13 +64,13 @@
 		//$mail->AddAttachment($_POST['fileSubmission']);
 		
 		$mail->send();
-		}
-		catch (Exception $e)
-		{
+	}
+	catch (Exception $e)
+	{
 		echo $e->errorMessage();
-		}
-		catch (\Exception $e)
-		{
+	}
+	catch (\Exception $e)
+	{
 		echo $e->getMessage();
 	}
 ?>
