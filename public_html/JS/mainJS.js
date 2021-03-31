0f439dabe149674c17dfc728c5f79444a7ad63f3
MineCopre <diogoah99@gmail.com>
2021-03-31 19:47:58 +0100

    testing captcha19

--- public_html/JS/mainJS.js
+++ public_html/JS/mainJS.js
@@ -1,13 +1,9 @@
 $(function(){	
 	
 	$('#contactForm').on('submit', function () {
-		e.preventDefault();
-		alert("teste");
 		grecaptcha.ready(function() {
-		alert("teste");
 			grecaptcha.execute('6LfmF8AZAAAAAFNT2ytbglM2hGpeJif1CmZgZZXm', {action:'submit'}).then(function(token) {
 				$("#g-recaptcha-response").value = token;
-				alert("teste2");
 			});
 		});
 	});
