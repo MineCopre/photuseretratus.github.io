76ba64ffa029aac3b2c46f1496f6198eddf7166e
diogoah99@gmail.com <diogoah99@gmail.com>
2021-09-04 12:51:31 +0100

    WEB APP

---
+++ webApp/src/main/java/pt/photuseretratus/webApp/Controller.java
@@ -0,0 +1,19 @@
+package pt.photuseretratus.webApp;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+
+    @CrossOrigin
+    @GetMapping("/geturl")
+    public String getURL(@RequestParam(value = "path", defaultValue = "/header.jpg") String path) throws IOException {
+        ImageKitIO imageKitIO = new ImageKitIO();
+        return imageKitIO.getURL(path);
+    }
+}
+
+
---
+++ webApp/src/main/java/pt/photuseretratus/webApp/ImageKitIO.java
@@ -0,0 +1,40 @@
+package pt.photuseretratus.webApp;
+
+import io.imagekit.sdk.ImageKit;
+import io.imagekit.sdk.config.Configuration;
+import io.imagekit.sdk.models.results.ResultList;
+import io.imagekit.sdk.utils.Utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ImageKitIO {
+
+    ImageKit imageKit;
+    Configuration configuration;
+    public ImageKitIO() throws IOException {
+        this.imageKit=ImageKit.getInstance();
+        configuration = new Configuration();
+        configuration.setPrivateKey(System.getenv("IMAGEKITIOPRIV"));
+        configuration.setPublicKey("public_TGL83sxiUWGZfYFL0MMz9r7AXTw=");
+        configuration.setUrlEndpoint("https://ik.imagekit.io/minecopre");
+        this.imageKit.setConfig(configuration);
+    }
+
+    public String getURL(String path){
+
+        List<Map<String, String>> transformation=new ArrayList<Map<String, String>>();
+
+        Map<String, Object> options=new HashMap<>();
+        options.put("path", path);
+        options.put("transformation",transformation);
+
+        System.out.println(imageKit.getUrl(options));
+        return imageKit.getUrl(options);
+
+    }
+
+}
